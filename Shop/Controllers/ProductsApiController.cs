6c3b47d8d3f3fad88f259da30ed9ea9e29e6ac99
JenafSanojan <93027811+JenafSanojan@users.noreply.github.com>
2024-05-08 16:02:22 +0530

    Tried adding apis, and minor changes

---
+++ Shop/Controllers/ProductsApiController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models.Entities;
+
+namespace Shop.Controllers
+{
+    public class ProductsApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ProductsApi
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+
+        // GET: ProductsApi/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // GET: ProductsApi/Ui
+        public IActionResult Ui()
+        {
+            return View();
+        }
+
+        // Create, Edit, Delete
+
+    }
+}
